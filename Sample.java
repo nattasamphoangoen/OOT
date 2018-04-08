8f6cc7c49df9dd8c5da07b698da8bdd872f8db13
nattasamphoangoen <natta151515@gmail.com>
2018-04-08 23:03:37 +0700

    add txt

---
+++ Sample.java
@@ -0,0 +1,12 @@
+packege comeng.sut;
+import java.util.Scanner;
+public class Sample{
+     public static void main(String[] args){
+	Scanner s = new Scanner(System.in);
+	System.out.print("Enter a number: ");
+	int a = s.nextInt();
+	
+	
+	
+     }
+}
